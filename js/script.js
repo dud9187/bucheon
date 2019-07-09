f7ef2a8fb3b361a7194a663ef4ae9ea35aa0f04c
dud9187 <52552035+dud9187@users.noreply.github.com>
2019-07-09 17:47:24 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(function(){
 
-   
+
+    
+    var swiper = new Swiper('.swiper-container', {
+        autoplay: {
+          delay: 2000,
+        },
+        loop:true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable:true,
+        },
+      });
+
+
 });
